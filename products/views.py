e71efe66a42341e0b7e1b02de924e74f2d2203cb
Carlos Eduardo Polidori Wendling <cadupolidori@gmail.com>
2024-08-16 16:38:38 -0300

    view function for /products

--- products/views.py
+++ products/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+# when the user access the /products, the 'view function' will be triggered
+def index(request):
+    return HttpResponse('Hello World')
