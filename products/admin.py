27e96487ba1800aa8f954237c8f72db2b8ace4b8
Carlos Eduardo Polidori Wendling <cadupolidori@gmail.com>
2024-08-19 09:29:41 -0300

    adding Offers model to admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Offer
 
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'stock')
 
 
-# command to administrate the Product class at the admin panel
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('code', 'discount')
+
+
+# command to administrate the model at the admin panel
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Offer, OfferAdmin)
