439230470c0880491bae7d7f483486c6386fc880
Carlos Eduardo Polidori Wendling <cadupolidori@gmail.com>
2024-08-19 09:59:16 -0300

    Passing the data to the index template

--- products/views.py
+++ products/views.py
@@ -5,7 +5,7 @@ from .models import Product
 # when the user access the index page (/products), the 'view function' will be triggered
 def index(request):
     products = Product.objects.all()
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'products': products})
 
 
 def new(request):
