6037f56d765c00d5043afe5a0302fc8590456a70
Carlos Eduardo Polidori Wendling <cadupolidori@gmail.com>
2024-08-19 09:06:45 -0300

    updating the admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Product
 
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+
+
 # command to administrate the Product class at the admin panel
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
