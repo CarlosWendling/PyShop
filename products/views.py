de418d5d7b25ec8e89b04aad67bf01f997540559
Carlos Eduardo Polidori Wendling <cadupolidori@gmail.com>
2024-08-19 09:42:16 -0300

    creating the index template for the products

--- products/views.py
+++ products/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
 # when the user access the index page (/products), the 'view function' will be triggered
 def index(request):
-    return HttpResponse('Hello World')
+    products = Product.objects.all()
+    return render(request, 'index.html')
 
 
 def new(request):
